62550d937e9c6ac68707d9b33d4bf5d14844b9cb
keitakn <keita.koga.work@gmail.com>
2020-08-30 15:27:46 +0900

    :art: #8 既存ユーザーへ認証コードを再送するパターンに対応する為、ResendCodeにもCustomMessageを設定

--- message/main_test.go
+++ message/main_test.go
@@ -68,6 +68,65 @@ func TestHandler(t *testing.T) {
 		}
 	})
 
+	// TriggerSourceが 'CustomMessage_ResendCode' の場合はCustomMessageが返却される
+	t.Run("Return ResendCode CustomMessage", func(t *testing.T) {
+		cc := &events.CognitoEventUserPoolsCallerContext{
+			AWSSDKVersion: "",
+			ClientID:      "",
+		}
+
+		ch := &events.CognitoEventUserPoolsHeader{
+			Version:       "",
+			TriggerSource: "CustomMessage_ResendCode",
+			Region:        "",
+			UserPoolID:    os.Getenv("TARGET_USER_POOL_ID"),
+			CallerContext: *cc,
+			UserName:      "keitakn",
+		}
+
+		ua := map[string]interface{}{
+			"sub": "dba1d5db-1d94-45b6-8f1b-fad23bb94cd5",
+		}
+
+		cm := map[string]string{
+			"key": "",
+		}
+
+		req := &events.CognitoEventUserPoolsCustomMessageRequest{
+			UserAttributes:    ua,
+			CodeParameter:     "123456789",
+			UsernameParameter: "keitakn",
+			ClientMetadata:    cm,
+		}
+
+		res := &events.CognitoEventUserPoolsCustomMessageResponse{
+			SMSMessage:   "SMSMessage",
+			EmailMessage: "EmailMessage",
+			EmailSubject: "EmailSubject",
+		}
+
+		ev := &events.CognitoEventUserPoolsCustomMessage{
+			CognitoEventUserPoolsHeader: *ch,
+			Request:                     *req,
+			Response:                    *res,
+		}
+
+		handlerResult, err := handler(*ev)
+		if err != nil {
+			t.Fatal("Error failed to trigger with an invalid request", err)
+		}
+
+		expected := &events.CognitoEventUserPoolsCustomMessageResponse{
+			SMSMessage:   "認証コードは {####} です。",
+			EmailMessage: "メールアドレスを検証するには、次のリンクをクリックしてください。 http://localhost:3900/cognito/signup/confirm?code=123456789&sub=keitakn",
+			EmailSubject: "サインアップ メールアドレスの確認をお願いします。",
+		}
+
+		if reflect.DeepEqual(&handlerResult.Response, expected) == false {
+			t.Error("\nActually: ", &handlerResult.Response, "\nExpected: ", expected)
+		}
+	})
+
 	// TriggerSourceが 'CustomMessage_SignUp' だがUserPoolIDが一致しないのでDefaultのメッセージが返却される
 	t.Run("Return Signup DefaultMessage Because the UserPoolID doesn't match", func(t *testing.T) {
 		cc := &events.CognitoEventUserPoolsCallerContext{
@@ -78,6 +137,65 @@ func TestHandler(t *testing.T) {
 		ch := &events.CognitoEventUserPoolsHeader{
 			Version:       "",
 			TriggerSource: "CustomMessage_SignUp",
+			Region:        "",
+			UserPoolID:    "OtherUserPoolID",
+			CallerContext: *cc,
+			UserName:      "keitakn",
+		}
+
+		ua := map[string]interface{}{
+			"sub": "dba1d5db-1d94-45b6-8f1b-fad23bb94cd5",
+		}
+
+		cm := map[string]string{
+			"key": "",
+		}
+
+		req := &events.CognitoEventUserPoolsCustomMessageRequest{
+			UserAttributes:    ua,
+			CodeParameter:     "123456789",
+			UsernameParameter: "keitakn",
+			ClientMetadata:    cm,
+		}
+
+		res := &events.CognitoEventUserPoolsCustomMessageResponse{
+			SMSMessage:   "SMSMessage",
+			EmailMessage: "EmailMessage",
+			EmailSubject: "EmailSubject",
+		}
+
+		ev := &events.CognitoEventUserPoolsCustomMessage{
+			CognitoEventUserPoolsHeader: *ch,
+			Request:                     *req,
+			Response:                    *res,
+		}
+
+		handlerResult, err := handler(*ev)
+		if err != nil {
+			t.Fatal("Error failed to trigger with an invalid request", err)
+		}
+
+		expected := &events.CognitoEventUserPoolsCustomMessageResponse{
+			SMSMessage:   "SMSMessage",
+			EmailMessage: "EmailMessage",
+			EmailSubject: "EmailSubject",
+		}
+
+		if reflect.DeepEqual(&handlerResult.Response, expected) == false {
+			t.Error("\nActually: ", &handlerResult.Response, "\nExpected: ", expected)
+		}
+	})
+
+	// TriggerSourceが 'CustomMessage_ResendCode' だがUserPoolIDが一致しないのでDefaultのメッセージが返却される
+	t.Run("Return ResendCode DefaultMessage Because the UserPoolID doesn't match", func(t *testing.T) {
+		cc := &events.CognitoEventUserPoolsCallerContext{
+			AWSSDKVersion: "",
+			ClientID:      "",
+		}
+
+		ch := &events.CognitoEventUserPoolsHeader{
+			Version:       "",
+			TriggerSource: "CustomMessage_ResendCode",
 			Region:        "",
 			UserPoolID:    "OtherUserPoolID",
 			CallerContext: *cc,
