40b82f8e1aaa6fb986c08258fd9545f42042f06a
keitakn <keita.koga.work@gmail.com>
2020-12-02 12:42:20 +0900

    :recycle: #26 Goの慣例に沿っていない変数名を修正、テストコードが肥大化していたのでリファクタリング

---
+++ message/test_helper.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"bytes"
+	"github.com/aws/aws-lambda-go/events"
+	"html/template"
+	"os"
+)
+
+// テスト用の期待値を作成する
+func createExpectedSignUpMessage(m SignUpMessage) (*bytes.Buffer, error) {
+	t := template.New("signup-template.html")
+
+	currentDir, _ := os.Getwd()
+	templatePath := currentDir + "/signup-template.html"
+
+	templates := template.Must(t.ParseFiles(templatePath))
+
+	var bodyBuffer bytes.Buffer
+	err := templates.Execute(&bodyBuffer, m)
+	if err != nil {
+		return nil, err
+	}
+
+	return &bodyBuffer, nil
+}
+
+// ForgotPasswordMessageカスタムメッセージのテスト用期待値を作成する
+func createExpectedForgotPasswordMessageMessage(m ForgotPasswordMessage) (*bytes.Buffer, error) {
+	t := template.New("forgot-password-template.html")
+
+	currentDir, _ := os.Getwd()
+	templatePath := currentDir + "/forgot-password-template.html"
+
+	templates := template.Must(t.ParseFiles(templatePath))
+
+	var bodyBuffer bytes.Buffer
+	err := templates.Execute(&bodyBuffer, m)
+	if err != nil {
+		return nil, err
+	}
+
+	return &bodyBuffer, nil
+}
+
+type createUserPoolsCustomMessageEventParams struct {
+	TriggerSource string
+	UserPoolID    string
+	UserName      string
+	Sub           string
+	CodeParameter string
+}
+
+func createUserPoolsCustomMessageEvent(p *createUserPoolsCustomMessageEventParams) *events.CognitoEventUserPoolsCustomMessage {
+	cc := &events.CognitoEventUserPoolsCallerContext{
+		AWSSDKVersion: "",
+		ClientID:      "",
+	}
+
+	ch := &events.CognitoEventUserPoolsHeader{
+		Version:       "",
+		TriggerSource: p.TriggerSource,
+		Region:        "",
+		UserPoolID:    p.UserPoolID,
+		CallerContext: *cc,
+		UserName:      p.UserName,
+	}
+
+	ua := map[string]interface{}{
+		"sub": p.Sub,
+	}
+
+	cm := map[string]string{
+		"key": "",
+	}
+
+	req := &events.CognitoEventUserPoolsCustomMessageRequest{
+		UserAttributes:    ua,
+		CodeParameter:     p.CodeParameter,
+		UsernameParameter: p.UserName,
+		ClientMetadata:    cm,
+	}
+
+	res := &events.CognitoEventUserPoolsCustomMessageResponse{
+		SMSMessage:   "SMSMessage",
+		EmailMessage: "EmailMessage",
+		EmailSubject: "EmailSubject",
+	}
+
+	ev := &events.CognitoEventUserPoolsCustomMessage{
+		CognitoEventUserPoolsHeader: *ch,
+		Request:                     *req,
+		Response:                    *res,
+	}
+
+	return ev
+}
