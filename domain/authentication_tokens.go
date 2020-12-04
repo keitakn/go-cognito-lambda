7fa45439602340efa2c7b23a9cf1e60f964a45c1
keitakn <keita.koga.work@gmail.com>
2020-12-05 00:40:36 +0900

    :bug: #13 DynamoDB用のタグの名前が間違っていたので修正

--- domain/authentication_tokens.go
+++ domain/authentication_tokens.go
@@ -1,8 +1,8 @@
 package domain
 
 type AuthenticationTokens struct {
-	Token          string `dynamo:"Token"`
-	CognitoSub     string `dynamo:"CognitoSub"`
-	SubscribeNews  bool   `dynamo:"SubscribeNews"`
-	ExpirationTime int64  `dynamo:"ExpirationTime"`
+	Token          string `dynamodbav:"Token"`
+	CognitoSub     string `dynamodbav:"CognitoSub"`
+	SubscribeNews  bool   `dynamodbav:"SubscribeNews"`
+	ExpirationTime int64  `dynamodbav:"ExpirationTime"`
 }
