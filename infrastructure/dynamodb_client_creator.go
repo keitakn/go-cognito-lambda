8b73153983b039926687cd412f2e1b7ac22f20e3
keitakn <keita.koga.work@gmail.com>
2020-12-04 15:04:33 +0900

    :art: #13 カスタムメッセージのLambdaの中で認証用のトークンを生成する処理を追加

---
+++ infrastructure/dynamodb_client_creator.go
@@ -0,0 +1,31 @@
+package infrastructure
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"os"
+)
+
+type DynamodbClientCreator struct{}
+
+func (c *DynamodbClientCreator) Create() *dynamodb.DynamoDB {
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	return dynamodb.New(sess)
+}
+
+func (c *DynamodbClientCreator) CreateTestClient() *dynamodb.DynamoDB {
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	return dynamodb.New(sess, &aws.Config{
+		Endpoint:    aws.String("http://localhost:58000"),
+		Region:      aws.String(os.Getenv("REGION")),
+		Credentials: credentials.NewStaticCredentials("dummy", "dummy", "dummy"),
+	})
+}
--- infrastructure/repository/dynamodb_authentication_token_repository.go
+++ infrastructure/repository/dynamodb_authentication_token_repository.go
@@ -5,6 +5,8 @@ import (
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"github.com/keitakn/go-cognito-lambda/domain"
+	"os"
+	"strings"
 )
 
 type DynamodbAuthenticationTokenRepository struct {
@@ -18,7 +20,7 @@ func (r *DynamodbAuthenticationTokenRepository) Create(item domain.Authenticatio
 	}
 
 	input := &dynamodb.PutItemInput{
-		TableName: aws.String("AuthenticationTokens"),
+		TableName: aws.String(strings.Title(os.Getenv("DEPLOY_STAGE")) + "AuthenticationTokens"),
 		Item:      av,
 	}
 
@@ -32,7 +34,7 @@ func (r *DynamodbAuthenticationTokenRepository) Create(item domain.Authenticatio
 
 func (r *DynamodbAuthenticationTokenRepository) FindByToken(token string) (*domain.AuthenticationTokens, error) {
 	input := &dynamodb.GetItemInput{
-		TableName: aws.String("AuthenticationTokens"),
+		TableName: aws.String(strings.Title(os.Getenv("DEPLOY_STAGE")) + "AuthenticationTokens"),
 		Key: map[string]*dynamodb.AttributeValue{
 			"Token": {
 				S: aws.String(token),
