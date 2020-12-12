441b86d60495d481d721092d9bcec60f356561e9
keitakn <keita.koga.work@gmail.com>
2020-12-12 15:48:25 +0900

    :sparkles: #13 DynamoDBに保存したトークンで認証を行うカスタム認証用の関数を追加

---
+++ authchallenge/create/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+// 「認証チャレンジの作成 Lambda」
+// event.Response.PublicChallengeParameters["challenge"] の値はクライアントに渡される
+// event.Response.PrivateChallengeParameters["answer"] の値は「認証チャレンジレスポンスの確認 Lambda」（authchallenge/verify/main.go）に渡される
+// https://docs.aws.amazon.com/ja_jp/cognito/latest/developerguide/user-pool-lambda-create-auth-challenge.html
+func Handler(event events.CognitoEventUserPoolsCreateAuthChallenge) (events.CognitoEventUserPoolsCreateAuthChallenge, error) {
+	event.Response.PublicChallengeParameters = map[string]string{}
+	event.Response.PrivateChallengeParameters = map[string]string{}
+
+	if event.Request.ChallengeName == "CUSTOM_CHALLENGE" {
+		event.Response.PublicChallengeParameters["challenge"] = "AuthenticationTokens"
+		event.Response.PrivateChallengeParameters["answer"] = event.UserName
+		event.Response.ChallengeMetadata = "CHALLENGE_AND_RESPONSE" + string(rune(len(event.Request.Session)))
+	}
+
+	return event, nil
+}
+
+func main() {
+	lambda.Start(Handler)
+}
---
+++ authchallenge/verify/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/keitakn/go-cognito-lambda/domain"
+	"github.com/keitakn/go-cognito-lambda/infrastructure"
+	"github.com/keitakn/go-cognito-lambda/infrastructure/repository"
+	"os"
+)
+
+var db *dynamodb.DynamoDB
+var authenticationTokenRepository domain.AuthenticationTokenRepository
+
+func init() {
+	dynamodbClientCreator := infrastructure.DynamodbClientCreator{}
+
+	if infrastructure.IsTestRun() {
+		db = dynamodbClientCreator.CreateTestClient()
+	} else {
+		db = dynamodbClientCreator.Create()
+	}
+
+	authenticationTokenRepository = &repository.DynamodbAuthenticationTokenRepository{Dynamodb: db}
+}
+
+func Handler(event events.CognitoEventUserPoolsVerifyAuthChallenge) (events.CognitoEventUserPoolsVerifyAuthChallenge, error) {
+	targetUserPoolId := os.Getenv("TARGET_USER_POOL_ID")
+	if targetUserPoolId != event.UserPoolID {
+		return event, nil
+	}
+
+	event.Response.AnswerCorrect = false
+
+	requestAuthenticationToken, ok := event.Request.ChallengeAnswer.(string)
+	if ok == false {
+		return event, nil
+	}
+
+	authenticationTokens, err := authenticationTokenRepository.FindByToken(requestAuthenticationToken)
+	if err != nil {
+		return event, err
+	}
+
+	userName := event.Request.PrivateChallengeParameters["answer"]
+	if userName == authenticationTokens.CognitoSub {
+		event.Response.AnswerCorrect = true
+		return event, nil
+	}
+
+	return event, nil
+}
+
+func main() {
+	lambda.Start(Handler)
+}
