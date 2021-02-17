ce0b73c7bec0322149d7b7b70bb75bb9e3df9def
keitakn <keita.koga.work@gmail.com>
2021-02-17 14:29:36 +0900

    :sparkles: #48 パスワードリセット用の認証メールを送信するAPIを実装

--- infrastructure/http_status_code.go
+++ infrastructure/http_status_code.go
@@ -3,6 +3,7 @@ package infrastructure
 const (
 	Ok                  int = 200
 	Created             int = 201
+	NoContent           int = 204
 	BadRequest          int = 400
 	NotFound            int = 404
 	InternalServerError int = 500
